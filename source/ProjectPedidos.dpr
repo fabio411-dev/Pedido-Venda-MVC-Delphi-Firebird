46f15f09da5cb9704220343a5c82e7a8e6a8df50
fabioakiyama <fabio.lara@multidadosti.com.br>
2026-03-03 00:11:51 -0300

    feat: atualização de units do projeto

--- source/ProjectPedidos.dpr
+++ source/ProjectPedidos.dpr
@@ -6,7 +6,13 @@ uses
   Models.Pedido in 'Models.Pedido.pas',
   Models.PedidoItem in 'Models.PedidoItem.pas',
   Models.Cliente in 'Models.Cliente.pas',
-  Models.Produto in 'Models.Produto.pas';
+  Models.Produto in 'Models.Produto.pas',
+  Repositories.ClienteRepository in 'Repositories.ClienteRepository.pas',
+  Repositories.ProdutoRepository in 'Repositories.ProdutoRepository.pas',
+  Repositories.PedidoRepository in 'Repositories.PedidoRepository.pas',
+  Services.PedidoService in 'Services.PedidoService.pas',
+  UDataModulePrincipal in 'UDataModulePrincipal.pas' {DataModulePrincipal: TDataModule},
+  uFrmPedido in 'uFrmPedido.pas' {FrmPedido};
 
 {$R *.res}
 
@@ -14,5 +20,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFPrincipalPedidos, FPrincipalPedidos);
+  Application.CreateForm(TDataModulePrincipal, DataModulePrincipal);
   Application.Run;
 end.
